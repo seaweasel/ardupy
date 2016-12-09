b820903202efbfa53a9b177979a975facdaa3d20
Hobo Noir <hobonetics@gmail.com>
2016-12-09 01:58:57 -0500

    update

--- ardupy/views.py
+++ ardupy/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/lights.jinj')
+@view_config(route_name='home', renderer='templates/lights.jinja2')
 def my_view(request):
     return {'project': 'ardupy'}
