6e6bb6414ae1c799728b601b2cad31d1bbd8e4d7
Hobo Noir <hobonetics@gmail.com>
2016-12-09 04:37:15 -0500

    fix

--- ardupy/views.py
+++ ardupy/views.py
@@ -2,32 +2,35 @@ from pyramid.view import view_config
 
 import simplejson as json
 
+
 def getLightState():
     with open('/home/ec2-user/lightstate', 'rb') as lights:
         return json.load(lights)
+
 
 def writeLightState(state):
     with open('/home/ec2-user/lightstate', 'wb') as lights:
         return json.dump(state, lights)
 
+
 @view_config(route_name='home', renderer='templates/lights.jinja2')
 def my_view(request):
 
-    #handle light state
+    # handle light state
     if request.method == "POST":
-        #handle light state
+        # handle light state
         state = getLightState()
 
         print request.POST
-        # lights = request.POST
-        # for light in lights.keys():
-        #     state[light] = lights[light]
-        #
-        # #save state
-        # writeLightState(state)
+        lights = request.POST
+        for light in lights.keys():
+            state[light] = lights[light]
+
+        #save state
+        writeLightState(state)
 
         return {"lights": state}
     else:
         # GET
-        #return light state
+        # return light state
         return {'lights': getLightState()}
